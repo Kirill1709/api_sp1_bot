44dc7023edc7a561ff73b0a11b6dbec1add6c3ca
Kirill1709 <kirill.gatsuk13@gmail.com>
2021-05-30 18:50:26 +0300

    Sprint 8. For second review

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import time
@@ -9,39 +10,53 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
-PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
+PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='program.log',
+    filemode='w',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 tg_logger.setup(logger, token=TELEGRAM_TOKEN, users=[CHAT_ID])
 
 
 def parse_homework_status(homework):
-    homework_name = homework["homework_name"]
+    status = {
+        'rejected': 'К сожалению в работе нашлись ошибки.',
+        'approved': ('Ревьюеру всё понравилось, можно '
+                     'приступать к следующему уроку.'),
+        'reviewing': 'работа взята в ревью.'
+    }
     try:
-        if homework["status"] == 'rejected':
-            verdict = 'К сожалению в работе нашлись ошибки.'
-        else:
-            verdict = ('Ревьюеру всё понравилось, можно '
-                       'приступать к следующему уроку.')
-        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-    except Exception as error:
-        logging.error(error, exc_info=True)
+        homework_name = homework["homework_name"]
+        return (f'У вас проверили работу "{homework_name}"!\n\n'
+                f'{status[homework["status"]]}')
+    except KeyError as error:
+        logger.error(f'Ключ {error} не найден', exc_info=True)
+        raise
+    except UnboundLocalError:
+        logger.error("Статус домашней работы не найден", exc_info=True)
+        raise
 
 
 def get_homework_statuses(current_timestamp):
     try:
         params = {'from_date': current_timestamp}
         homework_statuses = requests.get(
-            'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+            URL,
             headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'},
             params=params)
         return homework_statuses.json()
-    except Exception as error:
-        logger.error(error, exc_info=True)
+    except json.JSONDecodeError:
+        logger.error("Ответ не соотвествует формату json", exc_info=True)
+        raise
 
 
 def send_message(message, bot_client):
@@ -57,13 +72,14 @@ def main():
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(
                     new_homework.get('homeworks')[0]), bot_client)
+                logging.info("Message send")
             current_timestamp = new_homework.get(
                 'current_date', current_timestamp)
             time.sleep(300)
 
         except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
-            time.sleep(5)
+            logger.error(f'Бот столкнулся с ошибкой: {e}')
+            time.sleep(300)
 
 
 if __name__ == '__main__':
